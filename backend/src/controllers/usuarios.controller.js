fa7caecb9aa0788ffdaa7d2f1dbbf50999de9b54
c-alvares <cyrobrandaoalvares@gmail.com>
2023-05-04 08:01:42 -0300

    Backend - Finalizada e testada função de login

--- backend/src/controllers/usuarios.controller.js
+++ backend/src/controllers/usuarios.controller.js
@@ -7,41 +7,36 @@ require("dotenv").config();
 const prisma = new PrismaClient();
 
 const read = async (req, res) => {
-  let users = await prisma.usuarios.findMany();
+	let users = await prisma.usuarios.findMany();
 
-  res.status(200).json(users).end();
+	res.status(200).json(users).end();
 };
 
 const login = async (req, res) => {
-  const user = await prisma.usuarios.findMany({
-    where: {
-      senha: req.body.senha,
-    },
-    select: {
-      id: true,
-      perfil_id: true,
-    },
-  });
-  if (user.length > 0) {
-    jwt.sign(
-      user,
-      process.env.KEY,
-      { expiresIn: "30m" },
-      function (err, token) {
-        if (err == null) {
-          user["token"] = token;
-          res.status(200).json(user).end();
-        } else {
-          res.status(404).json(err).end();
-        }
-      }
-    );
-  } else {
-    res.status(404).json("Erro na tentativa de acesso").end();
-  }
+	const user = await prisma.usuarios.findMany({
+		where: {
+			senha: req.body.senha,
+		},
+		select: {
+			id: true,
+			perfil_id: true,
+		},
+	});
+	if (user.length > 0) {
+		jwt.sign(user[0], process.env.KEY, { expiresIn: "30m" }, function (err, token) {
+				if (err == null) {
+					user[0]["token"] = token;
+					res.status(200).json(user[0]).end();
+				} else {
+					res.status(401).json(err).end();
+				}
+		});
+	} else {
+		res.status(401).json("Erro na tentativa de acesso").end();
+	}
 };
 
 module.exports = {
-  read,
-  login,
+	read,
+	login,
 };
