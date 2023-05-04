feb51f273c1e8243ed68e4356934962f4089df39
c-alvares <cyrobrandaoalvares@gmail.com>
2023-05-04 08:44:53 -0300

    Backend - Identando middleware

--- backend/src/middleware/middleware.js
+++ backend/src/middleware/middleware.js
@@ -2,14 +2,12 @@ const jwt = require('jsonwebtoken');
 
 const acessValidator = (req, res, next) => {
     let token = req.headers.authorization;
-    // console.log(token)
     if(token) {
         token = token.split(' ')[1];
         jwt.verify(token, process.env.KEY, (err, data) => {
             if(err != null) res.status(404).json(err).end();
             else {
-                if(data.usuario.perfil === 2) {
-                    console.log(data)
+                if(data.perfil_id === 2) {
                     next();
                 }else {
                     res.status(401).end();
