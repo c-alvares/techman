63d04ac085bff770a8605f58ef6efc2c30ad5e33
c-alvares <cyrobrandaoalvares@gmail.com>
2023-05-05 11:38:09 -0300

    Implementado acesso para diferentes páginas.

--- frontend/acesso/acesso.js
+++ frontend/acesso/acesso.js
@@ -1,13 +1,57 @@
-const painel = document.querySelector('#painelDeSenha');
-const teclas = painel.querySelector('#tecladoVirtual');
+const inputSenha = document.getElementById("inputSenha");
+const botoesTeclado = document.querySelectorAll("#tecladoVirtual button");
+const btnTeclaEnter = document.querySelector(".teclaEnter");
 
+const limparSenha = () => {
+  inputSenha.value = "";
+//   habilitarTeclaEnter();
+};
 
-teclas.addEventListener('click', e => {
-    if (e.target.matchs('button')) {
-        const tecla = e.target
-        const action = tecla.dataset.action
-        if (action === 'acessar')  {
-            console.log('teste')
-        }
-    }
-})
+const adicionarNumeroSenha = (valor) => {
+  inputSenha.value += valor;
+  habilitarTeclaEnter();
+};
+
+const habilitarTeclaEnter = () => {
+  btnTeclaEnter.disabled = inputSenha.value.length !== 6;
+};
+
+botoesTeclado.forEach((botao) => {
+  const valor = botao.textContent;
+  if (valor === "C") {
+    botao.addEventListener("click", limparSenha);
+  } else {
+    botao.addEventListener("click", () => adicionarNumeroSenha(valor));
+  }
+});
+
+// btnTeclaEnter.addEventListener("click", () => {
+//   if (inputSenha.value.length !== 6) {
+//     alert("ERRO: Senha incorreta.");
+//     limparSenha();
+//   } else {
+//     acessarSistema();
+//   }
+// });
+
+const acessarSistema = () => {
+  const senha = inputSenha.value;
+  fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ senha }),
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        alert("ERRO: Senha incorreta.");
+        limparSenha();
+      } else {
+        return response.json();
+      }
+    })
+    .then((response) => {
+        if ( response.id !== 2) window.location.href = '../home/home.html';
+        else window.location.href = '../admin/admin.html';
+    })
+    .catch((err) => console.error(err));
+};
