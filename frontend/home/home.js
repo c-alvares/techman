4af138824425a6547f407249f474f48d2734f5ac
c-alvares <cyrobrandaoalvares@gmail.com>
2023-05-09 07:58:03 -0300

    Frontend - Alterado script para o home. Bug de tipo corrigido.

---
+++ frontend/home/home.js
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container");
+
+
+
+const options = { method: "GET" };
+
+fetch("http://localhost:3000/equipamentos", options)
+  .then((response) => response.json())
+  .then((resp) => {
+    resp.forEach((card) => {
+      if (card.ativo !== false) {
+        let cardEquipamento = container.cloneNode(true);
+        cardEquipamento.classList.remove("model");
+        cardEquipamento.querySelector('.imagem').src = "../../docs/assets/" + card.imagem
+        cardEquipamento.querySelector("h3").innerHTML = card.imagem;
+        cardEquipamento.querySelector("p").innerHTML = card.descricao;
+        cardEquipamento.innerHTML += `<img src="../../docs/assets/comentario.png" onclick="verComentarios(${card.id})">`
+        cardEquipamento.innerHTML += `<hr>`
+
+        document.querySelector("main").appendChild(cardEquipamento);
+        // appendar sempre a um elemento pai da variável definida(<main> <div>)
+      }
+    });
+  })
+  .catch((err) => console.error(err));
