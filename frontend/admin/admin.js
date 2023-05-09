b3da28c48ee38c142df93cf91a519ffc81a4a754
c-alvares <cyrobrandaoalvares@gmail.com>
2023-05-09 07:59:26 -0300

    Frontend - Função de listagem de equipamentos alocada no script admin

--- frontend/admin/admin.js
+++ frontend/admin/admin.js
@@ -1,9 +1,31 @@
-// id
-// equipamento
-// imagem
-// descricao
-// ativo
-// data
+const container = document.querySelector(".container");
+
+const perfilAdministrador = JSON.parse(localStorage.getItem('dados')).perfil_id
+
+
+const options = { method: "GET" };
+fetch("http://localhost:3000/equipamentos", options)
+  .then((response) => response.json())
+  .then((resp) => {
+    resp.forEach((card) => {
+      if (card.ativo !== false) {
+        let cardEquipamento = container.cloneNode(true);
+        cardEquipamento.classList.remove("model");
+        cardEquipamento.querySelector('.imagem').src = "../../docs/assets/" + card.imagem;
+        cardEquipamento.querySelector("h3").innerHTML = card.imagem;
+        cardEquipamento.querySelector("p").innerHTML = card.descricao;
+        cardEquipamento.innerHTML += `<img src="../../docs/assets/comentario.png" onclick="verComentarios(${card.id})">`;
+        if ( perfilAdministrador === 2 ) {
+          cardEquipamento.innerHTML += `<img src="../../docs/assets/deletar.png" onclick="excluir(${card.id})">`;
+        }
+        cardEquipamento.innerHTML += `<hr>`;
+        document.querySelector("main").appendChild(cardEquipamento);
+        // appendar sempre a um elemento pai da variável definida(<main> <div>)
+      }
+    });
+  })
+  .catch((err) => console.error(err));
+
 
 const excluir = (params) => {
     const options = {
@@ -25,3 +47,10 @@ const excluir = (params) => {
       })
       .catch(err => console.error(err));
 }
+
+// id
+// equipamento
+// imagem
+// descricao
+// ativo
+// data
